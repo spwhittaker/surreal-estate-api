a358e0cceb9607f8155793ef2cbad39c11b0acfb
Ersel Aker <erselaker@gmail.com>
2018-06-06 20:28:47 +0100

    set access-control header

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(function(req, res, next){
   whitelist.forEach(function(val, key){
     if (origin.indexOf(val) > -1){
       res.setHeader('Access-Control-Allow-Origin', origin);
+      res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     }
   })
 
