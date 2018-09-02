d2102bf406dca954d91f86a2f8517d39d55c7b93
Joe Stephens <joe@mcr.codes>
2018-09-02 20:24:47 +0100

    Property ref

--- models/favourite.js
+++ models/favourite.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const FavouriteSchema = new mongoose.Schema({
   fbUserId: { type: String, require: true },
-  propertyId: { type: String, require: true }
+  property: { type: Schema.Types.ObjectId, ref: 'Property', require: true }
 });
 
 const FavouriteModel = mongoose.model('Favourite', FavouriteSchema);
