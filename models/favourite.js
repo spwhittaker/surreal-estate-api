774c7dfbc79cc13200eeaab210ff4198f2ca565f
Joe Stephens <joe@mcr.codes>
2018-09-02 20:31:53 +0100

    PropertyListing

--- models/favourite.js
+++ models/favourite.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const FavouriteSchema = new mongoose.Schema({
   fbUserId: { type: String, require: true },
-  property: { type: Schema.Types.ObjectId, ref: 'Property', require: true }
+  propertyListing: { type: Schema.Types.ObjectId, ref: 'PropertyListing', require: true }
 });
 
 const FavouriteModel = mongoose.model('Favourite', FavouriteSchema);
